ade1984bae032d2268186bb05a644aacc873482b
kaurraman275 <kaurraman275@gmail.com>
2020-05-29 08:03:16 -0400

    Added dice-roller.js

---
+++ d2-focal/dice-roller.js
@@ -0,0 +1,12 @@
+function rnd(dice){
+    var number=Math.floor(Math.random(dice)*25);
+    }
+    
+    var dice = ["One" , "Two" , "Three" , "Four" , "Five" , "Six"];
+    
+    length = dice.length;
+    console.log(length);
+    
+    for (var i = 0; i < dice.length; i++) {
+    console.log(dice[i]);
+    }
