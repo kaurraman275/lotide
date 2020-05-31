8ed12d7d2bbfda4958ee70813aaacbc8ae1d2b64
kaurraman275 <kaurraman275@gmail.com>
2020-05-31 10:47:56 -0400

    added eqQbjects.js

---
+++ eqObjects.js
@@ -0,0 +1,24 @@
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+
+const eqObjects = function(object1, object2) {
+
+
+    object1.keys[Length] !== object2.keys[length]
+
+   
+
+    for (const property in eqObjects) {
+        if(objects1[property]===value   &&  object2[property] === value) 
+        
+        return true;
+        else(object1[property] !== value && object2[property] !== value)
+
+        return false;
+
+};
+
+eqObjects(ab, ba); 
+eqObjects(ab, abc);
+} 
