aeb1f25f97f762a378bd13b8be57f46d27c352c0
kaurraman275 <kaurraman275@gmail.com>
2020-05-30 20:01:42 -0400

    Added countPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,16 @@
+const letterPositions = function(sentence) {
+    const results = {};
+
+
+    for (let i = 0; i<sentence.length; i++)
+    {
+        var n = sentence.indexOf("g");
+    }
+    // logic to update results here
+    console.log(n);
+    return results;
+  };
+
+  letterPositions("lighthouse in the house")
+
+  
