c876cf8a2036cc5f28ca026d652668b71af7381d
kaurraman275 <kaurraman275@gmail.com>
2020-06-01 19:38:00 -0400

    added findKey.js

---
+++ findKey.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+
+    if(actual === expected) {
+        console.log("Assertion Passed: [actual] === [expected]");
+    }
+    else if (actual !== expected) {
+        console.log("Assertion Failed: [actual] !== [expected] ");
+    }
+
+};
+
+const findKey = function(obj, callback){
+
+    for(const key in obj){
+
+        var noma = (callback(obj[key]));
+
+        if (noma === true)
+        {
+            return key
+        }
+       
+    }
+}
+
+
+
+    const result = findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2)
+
+  console.log(result);
